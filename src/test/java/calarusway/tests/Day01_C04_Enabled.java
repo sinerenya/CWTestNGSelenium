970b46634bd65ce813e919aebf0d64ffc4e9f26b
Sinan Aydinli <sinerenya@gmail.com>
2023-01-01 13:30:05 +0300

    update

---
+++ src/test/java/calarusway/tests/Day01_C04_Enabled.java
@@ -0,0 +1,18 @@
+package calarusway.tests;
+
+import org.testng.annotations.Test;
+
+public class Day01_C04_Enabled {
+
+
+    @Test(enabled = true)
+    public void enabledTest(){
+        System.out.println("Enabled test");
+    }
+
+    @Test(enabled = false)
+    public void disabledTest(){
+        System.out.println("Disabled test");
+    }
+
+}
---
+++ src/test/java/calarusway/tests/Day01_C06_Description.java
@@ -0,0 +1,4 @@
+package calarusway.tests;
+
+public class Day01_C06_Description {
+}
